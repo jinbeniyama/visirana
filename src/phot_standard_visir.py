3018b255f9117400ebd2522c2742e158a0423e62
Jin Beniyama <beniyama@ioa.s.u-tokyo.ac.jp>
2024-10-16 22:39:35 +0200

    Fix typo

--- src/phot_standard_visir.py
+++ src/phot_standard_visir.py
@@ -304,7 +304,7 @@ if __name__ == "__main__":
     print(f"    Best flux C2 = {flux_best:.1f} ADU")
     print(f"    Exp time  C3 = {texp_standard} s")
     coeff = flux_cat/flux_best*texp_standard
-    print(f"    Coeff C1/C2*C3   = {coeff:.2f} mJy ADU/s")
+    print(f"    Coeff C1/C2*C3   = {coeff:.4f} mJy s/ADU")
 
     
     # Plot growth curve
